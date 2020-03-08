fc5e993d248f16c952001f77efae11c84347d4a4
leedavidcs <lee.david.cs@gmail.com>
2020-03-08 13:11:30 -0700

    Added helper to apply generators to where filters on nexus

--- src/server/prisma/generators/index.ts
+++ src/server/prisma/generators/index.ts
@@ -1,1 +1,12 @@
 export * from "./string-filter.generator";
+
+export const applyGenerators = (
+	whereInput: Maybe<Record<string, any>>,
+	generators: readonly (readonly Record<string, any>[])[]
+) => ({
+	...whereInput,
+	AND: [
+		...(whereInput?.AND || []),
+		...generators.reduce((acc, generator) => acc.concat(generator))
+	]
+});
