dddef19c62dc7455f6d6340e52a952e2adfc2575
leedavidcs <lee.david.cs@gmail.com>
2020-03-13 20:42:44 -0700

    Use mock provider on storybook decorator

--- src/client/storybook/decorators/with-root-provider.decorator.tsx
+++ src/client/storybook/decorators/with-root-provider.decorator.tsx
@@ -1,7 +1,12 @@
 import { RootProvider } from "@/client/components";
+import { MockApollo } from "@/client/graphql";
 import { DecoratorFunction } from "@storybook/addons";
 import React, { ReactElement } from "react";
 
 export const withRootProvider: DecoratorFunction<ReactElement> = (getStory) => {
-	return <RootProvider mockRequests={true}>{getStory()}</RootProvider>;
+	return (
+		<MockApollo>
+			<RootProvider>{getStory()}</RootProvider>
+		</MockApollo>
+	);
 };
