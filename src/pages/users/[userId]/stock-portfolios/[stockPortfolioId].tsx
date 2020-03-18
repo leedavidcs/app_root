bc810724c8561e74d37f09091dce805a3b4df6e6
leedavidcs <lee.david.cs@gmail.com>
2020-03-17 17:21:23 -0700

    Remove in-progress auth-guard on stock-portfolio page

--- src/pages/users/[userId]/stock-portfolios/[stockPortfolioId].tsx
+++ src/pages/users/[userId]/stock-portfolios/[stockPortfolioId].tsx
@@ -1,5 +1,3 @@
-import { GetOneStockPortfolioDocument, GetOneStockPortfolioQuery } from "@/client/graphql";
-import { withAuth } from "@/client/hocs";
 import { NextPage } from "next";
 import React from "react";
 
@@ -7,15 +5,4 @@ export const Page: NextPage = () => {
 	return <div>Stock portfolio page works~!</div>;
 };
 
-Page.getInitialProps = async ({ apolloClient, query }) => {
-	const { stockPortfolioId } = query;
-
-	await apolloClient.query<GetOneStockPortfolioQuery>({
-		query: GetOneStockPortfolioDocument,
-		variables: { id: stockPortfolioId }
-	});
-
-	return {};
-};
-
-export default withAuth()(Page);
+export default Page;
