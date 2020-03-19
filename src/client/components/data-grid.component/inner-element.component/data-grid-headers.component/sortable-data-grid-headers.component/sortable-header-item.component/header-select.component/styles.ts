867109aa8e902195b55794c73e7b8ecbac6ff40c
leedavidcs <lee.david.cs@gmail.com>
2020-03-19 14:27:15 -0700

    Change data-grid header-select to have limited height and scroll

--- src/client/components/data-grid.component/inner-element.component/data-grid-headers.component/sortable-data-grid-headers.component/sortable-header-item.component/header-select.component/styles.ts
+++ src/client/components/data-grid.component/inner-element.component/data-grid-headers.component/sortable-data-grid-headers.component/sortable-header-item.component/header-select.component/styles.ts
@@ -2,7 +2,10 @@ import { CustomTheme } from "@/client/themes";
 import { createUseStyles } from "react-jss";
 
 const styles = (theme: CustomTheme) => ({
-	root: {}
+	root: {
+		maxHeight: 200,
+		overflowY: "scroll"
+	}
 });
 
 export const useStyles = createUseStyles<CustomTheme, keyof ReturnType<typeof styles>>(styles);
