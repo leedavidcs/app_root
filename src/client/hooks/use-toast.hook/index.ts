c34d430ae2f19fe27b538ce047a9583fa69a7d3a
leedavidcs <lee.david.cs@gmail.com>
2020-03-22 21:59:21 -0700

    Fix use-toast failing during ssr

--- src/client/hooks/use-toast.hook/index.ts
+++ src/client/hooks/use-toast.hook/index.ts
@@ -1,9 +1,27 @@
 import { Classes, IToaster, Toaster } from "@blueprintjs/core";
 
-const toaster: IToaster = Toaster.create({
-	className: Classes.DARK,
-	maxToasts: 5,
-	position: "bottom-left"
-});
+let initialized = false;
+let toaster: IToaster = {
+	show: () => "",
+	dismiss: () => undefined,
+	clear: () => undefined,
+	getToasts: () => []
+};
 
-export const useToast = (): IToaster => toaster;
+export const useToast = (): IToaster => {
+	if (initialized) {
+		return toaster;
+	}
+
+	if (typeof window !== "undefined") {
+		toaster = Toaster.create({
+			className: Classes.DARK,
+			maxToasts: 5,
+			position: "bottom-left"
+		});
+
+		initialized = true;
+	}
+
+	return toaster;
+};
