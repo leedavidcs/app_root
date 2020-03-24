1e318dc66fedc355dcd9efb8e37fb99d7b8c4e03
leedavidcs <lee.david.cs@gmail.com>
2020-03-23 18:16:24 -0700

    Prevent errors with toast for ssr

--- src/client/hooks/use-toast.hook/index.ts
+++ src/client/hooks/use-toast.hook/index.ts
@@ -1,27 +1,17 @@
 import { Classes, IToaster, Toaster } from "@blueprintjs/core";
 
-let initialized = false;
-let toaster: IToaster = {
-	show: () => "",
-	dismiss: () => undefined,
-	clear: () => undefined,
-	getToasts: () => []
-};
+const toaster: IToaster =
+	typeof window !== "undefined"
+		? Toaster.create({
+				className: Classes.DARK,
+				maxToasts: 5,
+				position: "bottom-left"
+		  })
+		: {
+				show: () => "",
+				dismiss: () => undefined,
+				clear: () => undefined,
+				getToasts: () => []
+		  };
 
-export const useToast = (): IToaster => {
-	if (initialized) {
-		return toaster;
-	}
-
-	if (typeof window !== "undefined") {
-		toaster = Toaster.create({
-			className: Classes.DARK,
-			maxToasts: 5,
-			position: "bottom-left"
-		});
-
-		initialized = true;
-	}
-
-	return toaster;
-};
+export const useToast = (): IToaster => toaster;
