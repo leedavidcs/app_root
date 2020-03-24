53d2dde205d7f0407512576f70ab869cd6c6f068
leedavidcs <lee.david.cs@gmail.com>
2020-03-24 12:47:17 -0700

    Connect edit page-part to edit page

--- src/pages/users/[userId]/stock-portfolios/[stockPortfolioId]/edit.tsx
+++ src/pages/users/[userId]/stock-portfolios/[stockPortfolioId]/edit.tsx
@@ -1,24 +1,75 @@
 import {
 	GetOneStockPortfolioDocument,
 	GetOneStockPortfolioQuery,
-	GetOneStockPortfolioQueryVariables
+	GetOneStockPortfolioQueryResult,
+	GetOneStockPortfolioQueryVariables,
+	useGetOneStockPortfolioQuery
 } from "@/client/graphql";
 import { withAuth } from "@/client/hocs";
+import { StockPortfolioEdit } from "@/client/page-parts/users/[userId]/stock-portfolios/[stockPortfolioId]/stock-portfolio-display.component/edit";
+import { CustomTheme } from "@/client/themes";
+import { StockPortfolioWhereUniqueInput } from "@prisma/client";
 import HttpStatus from "http-status-codes";
 import { NextPage } from "next";
 import Error from "next/error";
-import React from "react";
+import { NextRouter, useRouter } from "next/router";
+import React, { useMemo } from "react";
+import { createUseStyles } from "react-jss";
 
 interface IInitialProps {
 	errorCode?: number;
+	errorTitle?: string;
 }
 
-export const Page: NextPage<IInitialProps> = ({ errorCode }) => {
+const styles = (theme: CustomTheme) => ({
+	root: {
+		maxWidth: 1280,
+		margin: "0 auto",
+		color: theme.onBackground
+	}
+});
+
+const useStyles = createUseStyles<CustomTheme, keyof ReturnType<typeof styles>>(styles);
+
+const useStockPortfolio = (): GetOneStockPortfolioQueryResult => {
+	const router: NextRouter = useRouter();
+
+	const { stockPortfolioId } = router.query;
+
+	const where = useMemo(
+		(): StockPortfolioWhereUniqueInput => ({ id: stockPortfolioId as string }),
+		[stockPortfolioId]
+	);
+
+	const result = useGetOneStockPortfolioQuery({ variables: { where } });
+
+	return result;
+};
+
+export const Page: NextPage<IInitialProps> = ({ errorCode, errorTitle }) => {
+	const classes = useStyles();
+
+	const { called, data, loading } = useStockPortfolio();
+
 	if (errorCode) {
-		return <Error statusCode={errorCode} />;
+		return <Error statusCode={errorCode} title={errorTitle} />;
 	}
 
-	return <div>Stock portfolio edit page works~!</div>;
+	const notFound: boolean = called && !loading && !data;
+
+	if (notFound) {
+		return <Error statusCode={HttpStatus.NOT_FOUND} title="Resource was not found" />;
+	}
+
+	return (
+		<main className={classes.root}>
+			{loading || !data?.stockPortfolio ? (
+				<p>loading...</p>
+			) : (
+				<StockPortfolioEdit stockPortfolio={data?.stockPortfolio} />
+			)}
+		</main>
+	);
 };
 
 Page.getInitialProps = async ({ apolloClient, query, user }) => {
@@ -39,13 +90,13 @@ Page.getInitialProps = async ({ apolloClient, query, user }) => {
 	const stockPortfolio = data.stockPortfolio;
 
 	if (!stockPortfolio) {
-		return { errorCode: HttpStatus.NOT_FOUND };
+		return { errorCode: HttpStatus.NOT_FOUND, errorTitle: "Resource was not found" };
 	}
 
 	const isCreator: boolean = user?.id === stockPortfolio.user.id;
 
 	if (!isCreator) {
-		return { errorCode: HttpStatus.FORBIDDEN };
+		return { errorCode: HttpStatus.FORBIDDEN, errorTitle: "Access to resource is forbidden" };
 	}
 
 	return {};
