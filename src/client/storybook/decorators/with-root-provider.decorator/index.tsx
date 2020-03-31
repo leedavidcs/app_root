52f2e49a3485c6824ffffb4adc138501ff3a960b
leedavidcs <lee.david.cs@gmail.com>
2020-03-30 20:24:05 -0700

    Update dependencies. Wrap storybook and app with stripe elements

--- src/client/storybook/decorators/with-root-provider.decorator/index.tsx
+++ src/client/storybook/decorators/with-root-provider.decorator/index.tsx
@@ -1,19 +1,26 @@
 import { ClickOutsideProvider, ModalProvider, StylesProvider } from "@/client/components";
 import { MockApollo } from "@/client/graphql";
 import { DecoratorFunction } from "@storybook/addons";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React, { ReactElement } from "react";
 import { MockNextRouter } from "./mock-next-router.component";
 
+const stripePublishable: string = process.env.STORYBOOK_STRIPE_PUBLISHABLE || "";
+const stripePromise = loadStripe(stripePublishable);
+
 export const withRootProvider: DecoratorFunction<ReactElement> = (getStory) => {
 	return (
-		<MockNextRouter>
-			<MockApollo>
-				<StylesProvider>
-					<ClickOutsideProvider>
-						<ModalProvider>{getStory()}</ModalProvider>
-					</ClickOutsideProvider>
-				</StylesProvider>
-			</MockApollo>
-		</MockNextRouter>
+		<Elements stripe={stripePromise}>
+			<MockNextRouter>
+				<MockApollo>
+					<StylesProvider>
+						<ClickOutsideProvider>
+							<ModalProvider>{getStory()}</ModalProvider>
+						</ClickOutsideProvider>
+					</StylesProvider>
+				</MockApollo>
+			</MockNextRouter>
+		</Elements>
 	);
 };
