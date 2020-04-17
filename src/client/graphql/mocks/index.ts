73f287c5e5e1ad9380ecf05edd3863439f80c834
leedavidcs <lee.david.cs@gmail.com>
2020-04-16 23:10:57 -0700

    Add mock for create stripe setup intent

--- src/client/graphql/mocks/index.ts
+++ src/client/graphql/mocks/index.ts
@@ -1,5 +1,6 @@
 import { MockedResponse } from "@apollo/react-testing";
 import { CreateStockPortfolioMock } from "./create-stock-portfolio.mock";
+import { CreateStripeSetupIntentMock } from "./create-stripe-setup-intent.mock";
 import { DeleteStockPortfolioMock } from "./delete-stock-portfolio.mock";
 import { GetDataKeyOptionsMock } from "./get-data-key-options.mock";
 import { GetManyStockPortfoliosMock } from "./get-many-stock-portfolios.mock";
@@ -10,6 +11,7 @@ import { SearchStockSymbolsMock } from "./search-stock-symbols.mock";
 
 export const mocks: readonly MockedResponse[] = [
 	CreateStockPortfolioMock,
+	CreateStripeSetupIntentMock,
 	DeleteStockPortfolioMock,
 	GetDataKeyOptionsMock,
 	GetManyStockPortfoliosMock,
