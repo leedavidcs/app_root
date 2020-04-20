0c3ab67df7cf1455d26c44f4d38bc8956e4bcc07
leedavidcs <lee.david.cs@gmail.com>
2020-04-20 06:08:00 -0700

    Added settings page placeholder

---
+++ src/pages/stock-portfolios/[stockPortfolioId]/settings/index.tsx
@@ -0,0 +1,36 @@
+import { GetOneStockPortfolioQuery } from "@/client/graphql";
+import { withStockPortfolioAuth } from "@/client/hocs";
+import { StockPortfolioHead } from "@/client/page-parts/stock-portfolios/[stockPortfolioId]";
+import { CustomTheme } from "@/client/themes";
+import { NextPage } from "next";
+import React from "react";
+import { createUseStyles } from "react-jss";
+
+type StockPortfolio = NonNullable<GetOneStockPortfolioQuery["stockPortfolio"]>;
+
+interface IProps {
+	stockPortfolio: StockPortfolio;
+}
+
+const styles = (theme: CustomTheme) => ({
+	root: {
+		color: theme.onBackground
+	},
+	head: {
+		marginBottom: 24
+	}
+});
+
+const useStyles = createUseStyles<CustomTheme, keyof ReturnType<typeof styles>>(styles);
+
+const Page: NextPage<IProps> = ({ stockPortfolio }) => {
+	const classes = useStyles();
+
+	return (
+		<main className={classes.root}>
+			<StockPortfolioHead stockPortfolio={stockPortfolio} />
+		</main>
+	);
+};
+
+export default withStockPortfolioAuth()(Page);
