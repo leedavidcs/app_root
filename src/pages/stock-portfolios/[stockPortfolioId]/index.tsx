59572fa39975245ecf0d8fd53b556aa1a041733f
leedavidcs <lee.david.cs@gmail.com>
2020-04-20 05:47:35 -0700

    Use hoc on stock-portfolio index page

--- src/pages/stock-portfolios/[stockPortfolioId]/index.tsx
+++ src/pages/stock-portfolios/[stockPortfolioId]/index.tsx
@@ -1,9 +1,5 @@
-import {
-	GetOneStockPortfolioQueryResult,
-	StockPortfolioWhereUniqueInput,
-	useGetOneStockPortfolioQuery
-} from "@/client/graphql";
-import { withAuth } from "@/client/hocs";
+import { GetOneStockPortfolioQuery } from "@/client/graphql";
+import { withStockPortfolioAuth } from "@/client/hocs";
 import {
 	StockPortfolioDisplay,
 	StockPortfolioHead
@@ -11,12 +7,15 @@ import {
 import { breakpoints, CustomTheme } from "@/client/themes";
 import { Classes } from "@blueprintjs/core";
 import classnames from "classnames";
-import HttpStatus from "http-status-codes";
 import { NextPage } from "next";
-import Error from "next/error";
-import { NextRouter, useRouter } from "next/router";
-import React, { useMemo } from "react";
+import React from "react";
 import { createUseStyles } from "react-jss";
+
+type StockPortfolio = NonNullable<GetOneStockPortfolioQuery["stockPortfolio"]>;
+
+interface IProps {
+	stockPortfolio: StockPortfolio;
+}
 
 const styles = (theme: CustomTheme) => ({
 	root: {
@@ -38,37 +37,8 @@ const styles = (theme: CustomTheme) => ({
 
 const useStyles = createUseStyles<CustomTheme, keyof ReturnType<typeof styles>>(styles);
 
-const useStockPortfolio = (): GetOneStockPortfolioQueryResult => {
-	const router: NextRouter = useRouter();
-
-	const { stockPortfolioId } = router.query;
-
-	const where = useMemo(
-		(): StockPortfolioWhereUniqueInput => ({ id: stockPortfolioId as string }),
-		[stockPortfolioId]
-	);
-
-	const result = useGetOneStockPortfolioQuery({ variables: { where } });
-
-	return result;
-};
-
-const Page: NextPage = () => {
+const Page: NextPage<IProps> = ({ stockPortfolio }) => {
 	const classes = useStyles();
-
-	const { called, data, loading } = useStockPortfolio();
-
-	const notFound: boolean = called && !loading && !data;
-
-	if (notFound) {
-		return <Error statusCode={HttpStatus.NOT_FOUND} title="Resource was not found" />;
-	}
-
-	const stockPortfolio = data?.stockPortfolio;
-
-	if (loading || !stockPortfolio) {
-		return null;
-	}
 
 	return (
 		<main className={classnames(Classes.DARK, classes.root)}>
@@ -78,4 +48,4 @@ const Page: NextPage = () => {
 	);
 };
 
-export default withAuth()(Page);
+export default withStockPortfolioAuth({ requireOwner: false })(Page);
