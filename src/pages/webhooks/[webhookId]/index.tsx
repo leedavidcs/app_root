72c90295bf969a6b07db5d67b3882bba28608a06
leedavidcs <lee.david.cs@gmail.com>
2020-04-21 01:52:42 -0700

    Use stock portfolio hoc to clean up webhook page

--- src/pages/webhooks/[webhookId]/index.tsx
+++ src/pages/webhooks/[webhookId]/index.tsx
@@ -1,21 +1,15 @@
 import { UpsertWebhookForm } from "@/client/forms";
-import {
-	GetOneStockPortfolioDocument,
-	GetOneStockPortfolioQuery,
-	GetOneStockPortfolioQueryVariables
-} from "@/client/graphql";
-import { withAuth } from "@/client/hocs";
+import { GetOneStockPortfolioQuery } from "@/client/graphql";
+import { withStockPortfolioAuth } from "@/client/hocs";
 import { CustomTheme } from "@/client/themes";
-import HttpStatus from "http-status-codes";
 import { NextPage } from "next";
-import Error from "next/error";
-import { NextRouter, useRouter } from "next/router";
 import React from "react";
 import { createUseStyles } from "react-jss";
 
-interface IInitialProps {
-	errorCode?: number;
-	errorTitle?: string;
+type StockPortfolio = NonNullable<GetOneStockPortfolioQuery["stockPortfolio"]>;
+
+interface IProps {
+	stockPortfolio: StockPortfolio;
 }
 
 const styles = (theme: CustomTheme) => ({
@@ -28,52 +22,14 @@ const styles = (theme: CustomTheme) => ({
 
 const useStyles = createUseStyles<CustomTheme, keyof ReturnType<typeof styles>>(styles);
 
-const Page: NextPage<IInitialProps> = ({ errorCode, errorTitle }) => {
+const Page: NextPage<IProps> = ({ stockPortfolio }) => {
 	const classes = useStyles();
-
-	const router: NextRouter = useRouter();
-
-	if (errorCode) {
-		return <Error statusCode={errorCode} title={errorTitle} />;
-	}
-
-	const { stockPortfolioId } = router.query;
 
 	return (
 		<main className={classes.root}>
-			<UpsertWebhookForm stockPortfolioId={stockPortfolioId as string} />
+			<UpsertWebhookForm stockPortfolioId={stockPortfolio.id} />
 		</main>
 	);
 };
 
-Page.getInitialProps = async ({ apolloClient, query, user }) => {
-	const { stockPortfolioId } = query;
-
-	const { data } = await apolloClient.query<
-		GetOneStockPortfolioQuery,
-		GetOneStockPortfolioQueryVariables
-	>({
-		query: GetOneStockPortfolioDocument,
-		variables: {
-			where: {
-				id: stockPortfolioId as string
-			}
-		}
-	});
-
-	const stockPortfolio = data.stockPortfolio;
-
-	if (!stockPortfolio) {
-		return { errorCode: HttpStatus.NOT_FOUND, errorTitle: "Resource was not found" };
-	}
-
-	const isCreator: boolean = user?.id === stockPortfolio.user.id;
-
-	if (!isCreator) {
-		return { errorCode: HttpStatus.FORBIDDEN, errorTitle: "Access to resource is forbidden" };
-	}
-
-	return {};
-};
-
-export default withAuth()(Page);
+export default withStockPortfolioAuth()(Page);
