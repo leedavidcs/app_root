52144ae0953de338862f16a9c768db8f3412fc58
leedavidcs <lee.david.cs@gmail.com>
2020-04-28 05:28:45 -0700

    Make upsert the only valid mutation for scheduled events

--- src/server/graphql/nexus/mutations/stock-portfolio-event.mutations/create-one-stock-portfolio-event.mutation.ts
+++ src/server/graphql/nexus/mutations/stock-portfolio-event.mutations/create-one-stock-portfolio-event.mutation.ts
@@ -1,8 +1,4 @@
-import { NotFoundError } from "@/server/utils";
-import { arg, inputObjectType, mutationField } from "@nexus/schema";
-import { ForbiddenError } from "apollo-server-micro";
-import { isNil } from "lodash";
-import { object } from "yup";
+import { inputObjectType } from "@nexus/schema";
 
 export const ScheduledEventCreatedaysInput = inputObjectType({
 	name: "ScheduledEventCreatedaysInput",
@@ -17,7 +13,6 @@ export const ScheduledEventCreateWithoutStockPortfolioEventInput = inputObjectTy
 		t.int("hour");
 		t.int("minute");
 		t.int("interval");
-		t.string("timezone");
 		t.field("days", { type: "ScheduledEventCreatedaysInput" });
 	}
 });
@@ -50,66 +45,6 @@ export const StockPortfolioEventCreateInput = inputObjectType({
 		t.field("stockPortfolio", {
 			type: "StockPortfolioCreateOneWithoutStockPortfolioEventInput",
 			nullable: false
-		});
-	}
-});
-
-export const createOneStockPortfolioEvent = mutationField("createOneStockPortfolioEvent", {
-	type: "StockPortfolioEvent",
-	nullable: false,
-	args: {
-		data: arg({ type: "StockPortfolioEventCreateInput", nullable: false })
-	},
-	authorize: async (parent, { data }, { prisma, user }) => {
-		if (!user) {
-			return false;
-		}
-
-		const stockPortfolio = await prisma.stockPortfolio.findOne({
-			where: data.stockPortfolio.connect
-		});
-
-		if (!stockPortfolio) {
-			return new NotFoundError("Stock-portfolio could not be found");
-		}
-
-		if (stockPortfolio.userId !== user.id) {
-			return new ForbiddenError(
-				"Cannot create an event for a stock-portfolio belonging to a different user"
-			);
-		}
-
-		return true;
-	},
-	yupValidation: () => ({
-		data: object().shape({
-			scheduledEvent: object().test({
-				message: "ScheduledEvent must either have interval, or explicit scheduling",
-				test: (value) => {
-					if (!isNil(value.interval)) {
-						return true;
-					}
-
-					if (!isNil(value.hour) && !isNil(value.minute) && value.days?.length > 0) {
-						return true;
-					}
-
-					return false;
-				}
-			})
-		})
-	}),
-	resolve: (parent, { data }, { prisma, user }) => {
-		return prisma.stockPortfolioEvent.create({
-			data: {
-				...data,
-				scheduledEvent: {
-					create: {
-						...data.scheduledEvent.create,
-						user: { connect: { id: user.id } }
-					}
-				}
-			}
 		});
 	}
 });
