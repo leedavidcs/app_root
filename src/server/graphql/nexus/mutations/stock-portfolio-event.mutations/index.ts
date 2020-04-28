3152e1d15cad16cf4e5bb1a488be86ea3a8c6d42
leedavidcs <lee.david.cs@gmail.com>
2020-04-28 06:04:48 -0700

    Add delete one stock-portfolio-event mutation

--- src/server/graphql/nexus/mutations/stock-portfolio-event.mutations/index.ts
+++ src/server/graphql/nexus/mutations/stock-portfolio-event.mutations/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-one-stock-portfolio-event.mutation";
+export * from "./delete-one-stock-portfolio-event.mutation";
 export * from "./update-one-stock-portfolio-event.mutation";
 export * from "./upsert-one-stock-portfolio-event.mutation";
