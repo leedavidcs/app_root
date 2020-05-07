2310bd26bfe4fe3b359ef770809a1b1ca15c991f
leedavidcs <lee.david.cs@gmail.com>
2020-05-07 16:14:16 -0700

    Added infinite loader list

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -6,6 +6,7 @@ export * from "./click-outside.component";
 export * from "./data-grid.component";
 export * from "./divider.component";
 export * from "./expansion-panel.component";
+export * from "./infinite-loader-list.component";
 export * from "./input.component";
 export * from "./interactable.component";
 export * from "./kebab-menu.component";
