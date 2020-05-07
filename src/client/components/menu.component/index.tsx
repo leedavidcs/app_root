f4ad41cf6f118732c55ab2c74fabe0e564ffd8a9
leedavidcs <lee.david.cs@gmail.com>
2020-05-06 23:54:13 -0700

    Add ul props to menu

--- src/client/components/menu.component/index.tsx
+++ src/client/components/menu.component/index.tsx
@@ -1,12 +1,12 @@
 import { IMenuDividerProps, Menu as BpMenu } from "@blueprintjs/core";
 import classnames from "classnames";
-import React, { FC, memo, ReactNodeArray, ReactText, useMemo } from "react";
+import React, { FC, HTMLAttributes, memo, ReactNodeArray, ReactText, useMemo } from "react";
 import { MenuContext } from "./context";
 import { MenuDivider } from "./menu-divider.component";
 import { IMenuItemProps, MenuItem } from "./menu-item.component";
 import { useStyles } from "./styles";
 
-interface IProps {
+interface IProps extends HTMLAttributes<HTMLUListElement> {
 	children?: ReactNodeArray;
 	className?: string;
 	activeItem?: ReactText;
@@ -18,14 +18,14 @@ interface IWithStaticProps {
 	Item: FC<IMenuItemProps>;
 }
 
-const _Menu: FC<IProps> = memo(({ activeItem, children, className, ulRef }) => {
+const _Menu: FC<IProps> = memo(({ activeItem, children, className, ulRef, ...restProps }) => {
 	const classes = useStyles();
 
 	const value = useMemo(() => ({ activeItem }), [activeItem]);
 
 	return (
 		<MenuContext.Provider value={value}>
-			<BpMenu className={classnames(classes.root, className)} ulRef={ulRef}>
+			<BpMenu className={classnames(classes.root, className)} ulRef={ulRef} {...restProps}>
 				{children}
 			</BpMenu>
 		</MenuContext.Provider>
