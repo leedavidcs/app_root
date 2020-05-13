c8e6c1d7a5d0bba07b5da349f433c2cc42791f77
leedavidcs <lee.david.cs@gmail.com>
2020-05-12 20:38:51 -0700

    Add pure-css theme changer script to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,57 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { oneLine } from "common-tags";
+import Document, {
+	DocumentContext,
+	DocumentInitialProps,
+	Head,
+	Html,
+	Main,
+	NextScript
+} from "next/document";
 import React, { Fragment } from "react";
 import { createGenerateId, JssProvider, SheetsRegistry } from "react-jss";
+
+const THEME_SCRIPT = oneLine`
+(function() {
+	window.__onThemeChange = function() { }
+
+	var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+	var darkQueryUpdate = function(e) {
+		document.body.className = e.matches ? "dark" : "light";
+	}
+
+	function setTheme(newTheme) {
+		window.__theme = newTheme;
+
+		if (newTheme === "system") {
+			darkQuery.addListener(darkQueryUpdate);
+
+			document.body.className = (darkQuery.matches ? "dark" : "light");
+		} else {
+			darkQuery.removeListener(darkQueryUpdate);
+
+			document.body.className = newTheme;
+		}
+
+		window.__onThemeChange(newTheme);
+	}
+
+	window.__setPreferredTheme = function(newTheme) {
+		setTheme(newTheme);
+		try {
+			localStorage.setItem("theme", newTheme);
+		} catch (err) { }
+	}
+
+	var initTheme;
+
+	try {
+		initTheme = localStorage.getItem("theme");
+	} catch (err) { }
+
+	setTheme(initTheme || "system");
+})();
+`;
 
 export default class extends Document {
 	static getInitialProps = async (ctx: DocumentContext) => {
@@ -29,5 +80,18 @@ export default class extends Document {
 				</Fragment>
 			)
 		};
+	};
+
+	render = () => {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<script dangerouslySetInnerHTML={{ __html: THEME_SCRIPT }} />
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	};
 }
