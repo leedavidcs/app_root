e99a3767204e9e991319db840c588f09ccde68ce
leedavidcs <lee.david.cs@gmail.com>
2020-05-16 23:30:59 -0700

    Fix bad apollo server on webhooks route. Remove context, since read-only

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,18 +1,17 @@
 import { getApolloServer } from "@/server/graphql";
-import { prisma } from "@/server/prisma";
 import { schema } from "@/server/webhooks";
+import { ApolloServer } from "apollo-server-micro";
 import { NextApiRequest, NextApiResponse } from "next";
-
-const server = getApolloServer({
-	schema,
-	context: () => ({ prisma }),
-	maxComplexity: 500,
-	maxDepth: 10
-});
 
 export const config = {
 	api: { bodyParser: false }
 };
+
+const server: ApolloServer = getApolloServer({
+	schema,
+	maxComplexity: 500,
+	maxDepth: 10
+});
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const handler = server.createHandler({ path: "/api/webhooks" });
