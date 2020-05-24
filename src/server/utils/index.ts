9d3da04522cc1a7adc3b13a16333e45efb6b7f26
leedavidcs <lee.david.cs@gmail.com>
2020-05-24 05:24:09 -0700

    Added pg utils

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -2,5 +2,6 @@ export * from "./errors.util";
 export * from "./general-functions.util";
 export * from "./get-base-url.util";
 export * from "./logger.util";
+export * from "./pg.util";
 export * from "./run-middleware.util";
 export * from "./stripe.util";
